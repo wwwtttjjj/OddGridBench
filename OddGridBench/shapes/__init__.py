22b2cb483e1f92f581602fdf25518031d3b77f92
wwwtttjjj <1019495562@qq.com>
2026-03-09 14:02:38 +0800

    update

--- OddGridBench/shapes/__init__.py
+++
@@ -1,5 +0,0 @@
-# shapes/__init__.py
-from .registry import draw_random_shape, draw_shape_by_name, shape_registry
-from . import svg_shapes
-
-__all__ = ["draw_random_shape", "draw_shape_by_name", "shape_registry"]
